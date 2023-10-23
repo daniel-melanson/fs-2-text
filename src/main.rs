b0c4693d04e935c21426e26e578abfff6d1db391
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-10-22 20:18:39 -0400

    feat: stub cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The path of the directory to print
+    path: Option<String>,
+
+    /// Print hidden files
+    #[arg(short, long)]
+    all: bool,
+
+    /// Recursive depth
+    #[arg(short, long, default_value = "1")]
+    depth: usize,
+
+    /// The maximum number of files to print under a directory
+    #[arg(long, default_value = "3")]
+    directory_length: usize,
+
+    /// Output the result as JSON
+    #[arg(short, long)]
+    json: bool,
+
+    /// Tab size
+    #[arg(short, long, default_value = "4")]
+    tab_size: usize,
+
+    /// The style of the output
+    #[arg(short, long, default_value = "ascii")]
+    style: Style,
+}
+
+#[derive(Copy, Clone, ValueEnum, Debug)]
+enum Style {
+    Ascii,
+    Emoji,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
